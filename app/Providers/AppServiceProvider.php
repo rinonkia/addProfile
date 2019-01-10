f1f43ff0ade70aefb68f76f9662b5aef34781324
gatapon <git@gatapon.net>
2019-01-10 21:46:37 +0900

    layouts.app css_ref secure_asset -> asset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
         \Schema::defaultStringLength(191);
 
         if (\App::environment('production')) {
-            \URL::environment('https');
+           \URL::environment('https');
         }
     }
 
