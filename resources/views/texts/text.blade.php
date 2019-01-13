d2a058b5fbe21b32beda53c378121749682c402f
gatapon <git@gatapon.net>
2019-01-13 19:20:23 +0900

    文言の修正

--- resources/views/texts/text.blade.php
+++ resources/views/texts/text.blade.php
@@ -10,18 +10,18 @@
         <ul>
             <li>
                 {!! Form::open(['route' => ['texts.destroy', $user->id], 'method' => 'delete']) !!}
-                    {!! Form::submit('削除しちゃう', ['class' => 'btn btn-danger btn-md']) !!}
+                    {!! Form::submit('削除', ['class' => 'btn btn-danger btn-md']) !!}
                 {!! Form::close() !!}
             </li>
             <li>
-                {!! link_to_route('texts.edit', '編集する', ['id' => $text->id], ['class' => 'btn btn-primary btn-md']) !!}
+                {!! link_to_route('texts.edit', '編集', ['id' => $text->id], ['class' => 'btn btn-primary btn-md']) !!}
             </li>
         </ul>
     @else
         <div class="text-muted">
             <p>現在テキストはありません</p>
         </div>
-        {!! link_to_route('texts.create', 'テキストを追加する', null, ['class' => 'btn btn-default']) !!}
+        {!! link_to_route('texts.create', 'テキストを追加', null, ['class' => 'btn btn-default']) !!}
     @endif
     </div>
 @endif
