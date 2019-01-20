b2aa12770963f6e34027ae9bcee49ab54b9329a1
gatapon <git@gatapon.net>
2019-01-20 12:15:41 +0900

    layouts.app separate under dev and production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,11 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-        <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+        @if(app('env') =='production')
+            <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+        @else
+            <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+        @endif
         
     </head>
     <body>
